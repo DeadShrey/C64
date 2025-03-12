f82fc1866f9a054afdaa19fa8c9f35ae1192bf4e
Shrey <gs.shreygupta@gmail.com>
2025-03-12 17:45:58 +0530

    Added move ordering to optimize pruning

--- src/C64v3.py
+++ src/C64v3.py
@@ -25,6 +25,28 @@ def evaluate(board: chess.Board):
     return evaluation * perspective
 
 
+def order_moves(move: chess.Move, board: chess.Board):
+    guess_score = 0
+    moved_piece = board.piece_type_at(move.from_square)
+    captured_piece = board.piece_type_at(move.to_square)
+    
+    if captured_piece is not None:
+        guess_score += 10 * piece_values[captured_piece] - piece_values[moved_piece]  # MVV-LVA
+    
+    elif board.is_en_passant(move):
+        guess_score += 10 - piece_values[moved_piece]  # The captured piece is a pawn
+    
+    else:
+        # The move is not a capture
+        if move.promotion is not None:
+            guess_score += 50
+        
+        elif board.is_attacked_by(not board.turn, move.to_square):
+            guess_score -= piece_values[moved_piece]
+    
+    return guess_score
+
+
 def search(board: chess.Board, depth: int, alpha: int, beta: int):
     if board.outcome() is not None:
         if board.is_checkmate():
@@ -34,8 +56,10 @@ def search(board: chess.Board, depth: int, alpha: int, beta: int):
     
     if depth == 0:
         return evaluate(board)
+    
+    ordered_moves = sorted(board.legal_moves, key=lambda move: order_moves(move, board), reverse=True)
 
-    for move in board.legal_moves:
+    for move in ordered_moves:
         board.push(move)
         evaluation = -search(board, depth - 1, -beta, -alpha)
         board.pop()
