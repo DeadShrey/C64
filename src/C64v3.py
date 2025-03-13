cf27c26310b276052b63bc3a4a5a6acbc5c4df37
Shrey <gs.shreygupta@gmail.com>
2025-03-13 18:29:17 +0530

    Added killer moves

--- src/C64v3.py
+++ src/C64v3.py
@@ -11,6 +11,8 @@ piece_values = {
     chess.KING: 0
 }
 
+killer_moves = [[None, None] for _ in range(100)]  # Storing 2 killer moves for each position
+
 mirrored_sq = tuple(chess.square_mirror(sq) for sq in range(64))  # Precomputing mirrored squares
 psqts = {
     chess.PAWN: (
@@ -107,7 +109,7 @@ def evaluate(board: chess.Board):
     return evaluation if board.turn == chess.WHITE else -evaluation  # Taking perspective into account
 
 
-def order_moves(move: chess.Move, board: chess.Board):
+def order_moves(move: chess.Move, board: chess.Board, depth: int):
     guess_score = 0
     moved_piece = board.piece_type_at(move.from_square)
     captured_piece = board.piece_type_at(move.to_square)
@@ -115,12 +117,12 @@ def order_moves(move: chess.Move, board: chess.Board):
     if captured_piece is not None:
         guess_score += 10 * piece_values[captured_piece] - piece_values[moved_piece]  # MVV-LVA
     
-    elif board.is_en_passant(move):
-        guess_score += 10 - piece_values[moved_piece]  # The captured piece is a pawn
-    
     else:
         # The move is not a capture
-        if move.promotion is not None:
+        if move in killer_moves[depth - 1]:
+            return 100000
+
+        if move.promotion:
             guess_score += 50
         
         elif board.is_attacked_by(not board.turn, move.to_square):
@@ -139,7 +141,7 @@ def search(board: chess.Board, depth: int, alpha: int, beta: int):
     if depth == 0:
         return evaluate(board)
     
-    ordered_moves = sorted(board.legal_moves, key=lambda move: order_moves(move, board), reverse=True)
+    ordered_moves = sorted(board.legal_moves, key=lambda move: order_moves(move, board, depth), reverse=True)
 
     for move in ordered_moves:
         board.push(move)
@@ -148,8 +150,12 @@ def search(board: chess.Board, depth: int, alpha: int, beta: int):
 
         alpha = max(alpha, evaluation)
         if alpha >= beta:
+            if not board.is_capture(move):  # The move is not a capture and can be used as a killer move
+                moves = killer_moves[depth - 1]
+                moves = [moves[1], move]
+
             return alpha  # *Snip*
-    
+
     return alpha
 
 
